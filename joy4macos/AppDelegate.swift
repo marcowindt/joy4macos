c43efe48d1189a521e41106da3111c1f8a0d73a0
Marco Dijkslag <m.dijkslag@student.utwente.nl>
2021-05-15 16:32:49 +0200

    Initial Commit

---
+++ joy4macos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  joy4macos
+//
+//  Created by Marco Dijkslag on 15/05/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ joy4macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  joy4macos
+//
+//  Created by Marco Dijkslag on 15/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
